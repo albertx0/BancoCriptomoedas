cacd3b5074a83482ab2d23004772f5add07a9900
Gabriel Albertini Pinheiro <pinheiro.gabriel2020@gmail.com>
2024-05-10 11:33:03 -0300

    Subindo projeto

---
+++ src/bancocriptomoedas/BancoCriptomoedas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bancocriptomoedas;
+
+import view.JanelaDeposito;
+import view.JanelaLogin;
+import view.JanelaMenu;
+
+/**
+ *
+ * @author albert
+ */
+public class BancoCriptomoedas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaLogin j = new JanelaLogin();
+        j.setVisible(true);
+    }
+    
+}
